34ac9250e55a66f23f61795bb2e8a4d73dfd8400
lihinimaleesha <lihinimaleesha4@gmail.com>
2023-10-22 21:38:12 +0530

    testing code removed

--- src/App.js
+++ src/App.js
@@ -42,7 +42,6 @@ function App() {
   return (
     <div className="container">
       <h1>Todo List</h1>
-      <h1>Todo List</h1>
       <div className="header text-center">
         <div className="container">
           <div className='inputpart'>
